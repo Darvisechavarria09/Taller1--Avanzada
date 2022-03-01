e8c2e6b99e0f12a3d64fa426cbcd6c1eb1c3d9ef
Darvisechavarria <yerikechavarria210@gmail.com>
2022-03-01 15:04:16 -0500

    TALLER solucionando el taller1

---
+++ temperatura.js
@@ -0,0 +1,4 @@
+//nombre funcion, parametros, resolver
+let calcularTemperatura =(temperaturaMaxima, temperaturaMinima) =>
+promedio=((temperaturaMaxima + temperaturaMinima)/2)
+console.log(calcularTemperatura(800,40))
